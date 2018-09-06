81dea25c3d56a402ec81515289dedadd5b46978e
Sasha122 <33477105+Sasha122@users.noreply.github.com>
2018-09-06 08:44:51 +0300

    MakeSomeChanges
    MakeSomeChanges

--- Project/config/config.py
+++ Project/config/config.py
@@ -1,8 +1,9 @@
 version = 1.0
 project_name = "PROJECT"
-theme = "dark";
+theme = "dark"
 settings = {
     'title' : "MySite",
     'user_name' : "admin" ,
     'user_password' : "123",
+    'footer' : "true"
 }
